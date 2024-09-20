279df6041f25b0b2db922551a06c2f817c74813a
AdrianVega1 <100384007+AdrianVega1@users.noreply.github.com>
2024-09-20 14:20:23 -0600

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World! Esto es una pruebaaaaaaaaaaaaaaaa')
+  res.send('Hello World! Pichichi es maricon')
 })
 
 app.listen(port, () => {
